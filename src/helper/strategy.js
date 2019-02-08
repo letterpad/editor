12da47b10897c7e94fc7918b5c50d951e77c2eec
Abhishek Saha <abhisheksaha11@gmail.com>
2019-02-08 15:16:38 +0100

    refactored the list utils

--- src/helper/strategy.js
+++ src/helper/strategy.js
@@ -15,3 +15,25 @@ export const isMarkActive = (value, type) => {
 export const hasBlock = (value, type) => {
     return value.blocks.some(node => node.type == type);
 };
+
+export const hasParentOfType = (value, type) => {
+    return value.blocks.some(
+        block =>
+            !!value.document.getClosest(
+                block.key,
+                parent => parent.type === type
+            )
+    );
+};
+export const getBlockParent = (value, type) => {
+    let parentNode = value.anchorBlock;
+    do {
+        if (parentNode.type === type) {
+            return parentNode;
+        }
+    } while ((parentNode = value.document.getParent(parentNode.key)));
+    return null;
+};
+export const getNodeOfType = (value, type) => {
+    return value.blocks.filter(block => block.type === type).first();
+};
--- src/plugins/list/ListUtils.js
+++ src/plugins/list/ListUtils.js
@@ -1,14 +1,10 @@
-import { hasBlock } from "../../helper/strategy";
-
-export const getBlockParent = (value, type) => {
-    let parentNode = value.anchorBlock;
-    do {
-        if (parentNode.type === type) {
-            return parentNode;
-        }
-    } while ((parentNode = value.document.getParent(parentNode.key)));
-    return null;
-};
+import {
+    hasBlock,
+    hasParentOfType,
+    getNodeOfType,
+    getBlockParent
+} from "../../helper/strategy";
+const itemType = "list-item";
 
 export const onTab = (event, change) => {
     event.preventDefault();
@@ -18,11 +14,11 @@ export const onTab = (event, change) => {
 
 export const onBackspace = (event, editor, next) => {
     const { value } = editor;
-    const isList = hasBlock(value, "list-item");
+    const isList = hasBlock(value, itemType);
     if (!isList) return next();
 
     if (value.selection.start.offset === 0) {
-        const node = getNodeOfType(value, "list-item");
+        const node = getNodeOfType(value, itemType);
         if (!node) return;
         const depth = value.document.getDepth(node.key);
         if (depth > 2) {
@@ -42,97 +38,97 @@ export const onBackspace = (event, editor, next) => {
 
 export const onEnter = (event, editor, next) => {
     const { value } = editor;
-
-    if (!hasBlock(value, "list-item")) return next();
-
+    const isList = hasBlock(value, itemType);
     const { startText } = value;
-    const isList = hasBlock(value, "list-item");
 
-    if (isList) {
-        event.preventDefault();
-        if (startText.text === "") {
-            deepRemoveList(editor).splitBlock();
-            // return editor
-            //     .splitBlock()
-            //     .setBlocks("paragraph")
-            //     .unwrapBlock("ordered-list")
-            //     .unwrapBlock("unordered-list");
-        } else {
-            return editor.setBlocks("list-item");
-        }
+    if (!isList) return next();
+
+    event.preventDefault();
+
+    if (startText.text === "") {
+        return deepRemoveList(editor);
+    } else {
+        return editor.splitBlock().setBlocks(itemType);
     }
-    next();
 };
 
-//....................... refactor belows
+// helper functions
 
-export const isList = value =>
-    value.blocks.some(block => block.type === "list-item");
+export const isUnorderedList = value => {
+    return hasParentOfType(value, "unordered-list");
+};
 
-export const hasParentOfType = (value, type) =>
-    value.blocks.some(
-        block =>
-            !!value.document.getClosest(
-                block.key,
-                parent => parent.type === type
-            )
-    );
-export const isUnorderedList = value =>
-    hasParentOfType(value, "unordered-list");
+export const isOrderedList = value => {
+    return hasParentOfType(value, "ordered-list");
+};
 
-export const isOrderedList = value => hasParentOfType(value, "ordered-list");
+export const getUnorderedListNode = value => {
+    return getNodeOfType(value, "unordered-list");
+};
 
-export const getNodeOfType = (value, type) =>
-    value.blocks.filter(block => block.type === type).first();
+export const getOrderedListNode = value => {
+    return getNodeOfType(value, "ordered-list");
+};
 
-export const getUnorderedListNode = value =>
-    getNodeOfType(value, "unordered-list");
-
-export const getOrderedListNode = value => getNodeOfType(value, "ordered-list");
-
-export const removeUnorderedList = editor =>
+export const removeUnorderedList = editor => {
     editor
         .setBlocks("paragraph")
         .unwrapBlock("unordered-list")
         .focus();
+};
 
-export const switchToOrderedList = editor =>
-    editor
+export const switchToOrderedList = editor => {
+    return editor
         .unwrapBlock("unordered-list")
         .wrapBlock("ordered-list")
         .focus();
+};
 
-export const removeOrderedList = editor =>
-    editor
+export const removeOrderedList = editor => {
+    return editor
         .setBlocks("paragraph")
         .unwrapBlock("ordered-list")
         .focus();
+};
 
-export const switchToUnorderedList = editor =>
-    editor
+export const switchToUnorderedList = editor => {
+    return editor
         .wrapBlock("unordered-list")
         .unwrapBlock("ordered-list")
         .focus();
+};
 
-export const applyList = (editor, type) =>
-    editor
-        .setBlocks("list-item")
+export const applyList = (editor, type) => {
+    return editor
+        .setBlocks(itemType)
         .wrapBlock(type)
         .focus();
+};
 
-export const onlyRemove = (change, type) => change.unwrapBlock(type).focus();
-export const onlyRemoveUnorderedList = change =>
-    onlyRemove(change, "unordered-list");
-export const onlyRemoveOrderedList = change =>
-    onlyRemove(change, "ordered-list");
+export const onlyRemove = (editor, type) => {
+    return editor.unwrapBlock(type).focus();
+};
 
-export const applyUnorderedList = change => applyList(change, "unordered-list");
-export const applyOrderedList = change => applyList(change, "ordered-list");
+export const onlyRemoveUnorderedList = editor => {
+    return onlyRemove(editor, "unordered-list");
+};
+
+export const onlyRemoveOrderedList = editor => {
+    return onlyRemove(editor, "ordered-list");
+};
+
+export const applyUnorderedList = editor => {
+    return applyList(editor, "unordered-list");
+};
+
+export const applyOrderedList = editor => {
+    return applyList(editor, "ordered-list");
+};
 
 const deepRemoveList = editor => {
     const { value } = editor;
     const { document } = value;
-    const node = getNodeOfType(value, "list-item");
+    const node = getNodeOfType(value, itemType);
     const depth = document.getDepth(node.key);
 
     Array(depth)
@@ -147,30 +143,28 @@ const deepRemoveList = editor => {
 
 export const unorderedListStrategy = editor => {
     const { value } = editor;
-    if (!isList(value)) return applyList(editor, "unordered-list");
+    if (!hasBlock(value, itemType)) return applyList(editor, "unordered-list");
 
     if (isUnorderedList(value)) return deepRemoveList(editor);
     if (isOrderedList(value)) return switchToUnorderedList(editor);
-    console.info("[SlateJS][ListPlugin] It is a different type of list.");
     return editor;
 };
 
 export const orderedListStrategy = editor => {
     const { value } = editor;
     // If it is not a list yet, transform it!
-    if (!isList(value)) return applyList(editor, "ordered-list");
+    if (!hasBlock(value, itemType)) return applyList(editor, "ordered-list");
 
     // If it is already a list, handle it!
     if (isOrderedList(value)) return deepRemoveList(editor);
     else if (isUnorderedList(value)) return switchToOrderedList(editor);
-    else console.info("[SlateJS][ListPlugin] It is a different type of list.");
     return editor;
 };
 
 export const increaseListDepthStrategy = editor => {
     const { value } = editor;
     // If it is not a list, kill the action immediately.
-    if (!isList(value)) return editor;
+    if (!hasBlock(value, itemType)) return editor;
 
     if (isUnorderedList(value)) return applyUnorderedList(editor);
     if (isOrderedList(value)) return applyOrderedList(editor);
@@ -180,9 +174,9 @@ export const increaseListDepthStrategy = editor => {
 export const decreaseListDepthStrategy = editor => {
     const { value } = editor;
     // If it is not a list, kill the action immediately.
-    if (!isList(value)) return editor;
+    if (!hasBlock(value, itemType)) return editor;
 
-    const node = getNodeOfType(value, "list-item");
+    const node = getNodeOfType(value, itemType);
     const depth = value.document.getDepth(node.key);
     if (isUnorderedList(value) && depth > 2)
         return onlyRemoveUnorderedList(editor);
