aa23596d8c830794e359d8a8b9941d1e654da2ec
Boopathi Rajaa <me@boopathi.in>
2019-02-23 16:52:22 +0100

    sort lines ascending

--- src/plugins/pluginConfigs.ts
+++ src/plugins/pluginConfigs.ts
@@ -1,5 +1,4 @@
 import { default as audioConfig } from "./audio/config";
-import { default as italicConfig } from "./italic/config";
 import { default as autoscrollConfig } from "./autoscroll/config";
 import { default as blockquoteConfig } from "./blockquote/config";
 import { default as boldConfig } from "./bold/config";
@@ -7,6 +6,7 @@ import { default as codeblockConfig } from "./codeblock/config";
 import { default as headingsConfig } from "./headings/config";
 import { default as highlightConfig } from "./highlight/config";
 import { default as imageConfig } from "./image/config";
+import { default as italicConfig } from "./italic/config";
 import { default as linebreakConfig } from "./linebreak/config";
 import { default as linkConfig } from "./link/config";
 import { default as listConfig } from "./list/config";
