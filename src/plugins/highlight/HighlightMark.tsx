6799303808bb8c3e747737ff09365a8ea573c900
Abhishek Saha <abhisheksaha11@gmail.com>
2019-02-24 12:52:27 +0100

    added code serializer

--- src/plugins/highlight/HighlightMark.tsx
+++ src/plugins/highlight/HighlightMark.tsx
@@ -1,6 +1,8 @@
 import React, { SFC } from "react";
 
 /* eslint-disable react/prop-types */
-const highlightMark: SFC = ({ children }) => <code>{children}</code>;
+const highlightMark: SFC = ({ children }) => {
+  return <code>{children}</code>;
+};
 
 export default highlightMark;
