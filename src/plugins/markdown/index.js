5cc4e11e6abeecaef474544d2ead51ea39a38978
Boopathi Rajaa <boopathi@users.noreply.github.com>
2019-03-01 22:24:53 +0100

    format all with prettier (#28)

--- src/plugins/markdown/index.js
+++ src/plugins/markdown/index.js
@@ -3,10 +3,10 @@ import * as MarkdownUtils from "./MarkdownUtils";
 
 /* eslint-disable no-unused-vars */
 const MarkdownPlugin = options => ({
-    onKeyDown(event, editor, next) {
-        return next();
-        // return MarkdownKeyboardShortcut(...args);
-    }
+  onKeyDown(event, editor, next) {
+    return next();
+    // return MarkdownKeyboardShortcut(...args);
+  }
 });
 
 export { MarkdownPlugin, MarkdownKeyboardShortcut, MarkdownUtils };
