eb59a89d194bbcc3245736fd428d755f09da6b72
Boopathi Rajaa <me@boopathi.in>
2019-03-03 17:07:17 +0100

    fix: circle ci build

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,15 @@
+let args = [];
+if (process.env.CHROME_FLAGS) {
+  args = process.env.CHROME_FLAGS.split(" ");
+}
+
 module.exports = {
   server: {
     command: "node test-utils/server.js",
     port: 4343
   },
   launch: {
+    args,
     headless: true
   },
   browserContext: "default"
