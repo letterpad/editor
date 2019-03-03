dcc3a44a6722fd4491f2dcbded289e99c1d336c2
Boopathi Rajaa <me@boopathi.in>
2019-03-03 17:27:21 +0100

    e2e on travis; unit on circle

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,17 +1,9 @@
-let args = [];
-if (process.env.CHROME_FLAGS) {
-  args = process.env.CHROME_FLAGS.split(" ");
-}
-
-console.log("using chrome flags", args);
-
 module.exports = {
   server: {
     command: "node test-utils/server.js",
     port: 4343
   },
   launch: {
-    args,
     headless: true
   },
   browserContext: "default"
