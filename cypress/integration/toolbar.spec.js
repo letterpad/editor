b3a0705d262f7fa328bd89f97b7877cde468ce59
Abhishek Saha <abhisheksaha11@gmail.com>
2019-04-15 18:08:12 +0200

    fixed toolbar tests

--- cypress/integration/toolbar.spec.js
+++ cypress/integration/toolbar.spec.js
@@ -7,48 +7,60 @@ context("Toolbar", () => {
   beforeEach(() => {
     cy.visit(params.testServer);
     cy.get('div[contenteditable="true"]');
-    clearEditor().then(() => {
-      cy.get("#letterpad-editor-toolbar-toggle-button").click();
-    });
   });
 
   it("tests heading", () => {
-    clickXPath("//span[contains(text(), 'looks_one')]");
-    cy.get('div[contenteditable="true"]')
-      .type("heading")
-      .then(() => {
-        getHtmlContents().then(res => {
-          cy.wrap(res).snapshot();
+    clearEditor().then(() => {
+      cy.get("#letterpad-editor-toolbar-toggle-button").click();
+
+      clickXPath("//span[contains(text(), 'looks_one')]");
+      cy.get('div[contenteditable="true"]')
+        .type("heading")
+        .then(() => {
+          getHtmlContents().then(res => {
+            cy.wrap(res).snapshot();
+          });
         });
-      });
+    });
   });
 
   it("tests audio", () => {
-    clickXPath("//span[contains(text(), 'queue_music')]");
-    cy.focused()
-      .type("foo.mp3{enter}")
-      .then(() => {
-        getHtmlContents().then(res => {
-          cy.wrap(res).snapshot();
+    clearEditor().then(() => {
+      cy.get("#letterpad-editor-toolbar-toggle-button").click();
+
+      clickXPath("//span[contains(text(), 'queue_music')]");
+      cy.focused()
+        .type("foo.mp3{enter}")
+        .then(() => {
+          getHtmlContents().then(res => {
+            cy.wrap(res).snapshot();
+          });
         });
-      });
+    });
   });
 
   it("tests image", () => {
-    clickXPath("//span[contains(text(), 'image')]");
-    cy.focused()
-      .type("foo.img{enter}")
-      .then(() => {
-        getHtmlContents().then(res => {
-          cy.wrap(res).snapshot();
+    clearEditor().then(() => {
+      cy.get("#letterpad-editor-toolbar-toggle-button").click();
+      clickXPath("//span[contains(text(), 'image')]");
+      cy.focused()
+        .type("foo.img{enter}")
+        .then(() => {
+          getHtmlContents().then(res => {
+            cy.wrap(res).snapshot();
+          });
         });
-      });
+    });
   });
 
   it("tests horizontal line", () => {
-    clickXPath("//span[contains(text(), 'more_horiz')]");
-    getHtmlContents().then(res => {
-      cy.wrap(res).snapshot();
+    clearEditor().then(() => {
+      cy.get("#letterpad-editor-toolbar-toggle-button").click();
+
+      clickXPath("//span[contains(text(), 'more_horiz')]");
+      getHtmlContents().then(res => {
+        cy.wrap(res).snapshot();
+      });
     });
   });
 });
