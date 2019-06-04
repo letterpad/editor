98d8410096b602b8d8abd154c01ecb159ca4c954
Boopathi Rajaa <me@boopathi.in>
2019-06-04 02:27:18 +0200

    Add demo bundle to ci

---
+++ webpack.config.demo.js
@@ -0,0 +1,12 @@
+const prodWebpackConfig = require("./webpack.config.prod");
+const { externals, ...withReact } = prodWebpackConfig;
+
+module.exports = {
+  ...withReact,
+  entry: "./index.tsx",
+  output: {
+    ...withReact.output,
+    path: __dirname + "/dist/bundles",
+    filename: "editor.demo.js"
+  }
+};
